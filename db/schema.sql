ce88b4650654d658458265a39e1ea9755aa0f791
jermz678 <jermz678@live.com>
2021-05-24 21:38:51 -0500

    working on creating database correctly

--- db/schema.sql
+++ db/schema.sql
@@ -4,22 +4,19 @@ DROP TABLE IF EXISTS department;
 
 
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
-  CONSTRAINT fk_party FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE SET NULL,
-     CONSTRAINT fk_party FOREIGN KEY (manager_id) REFERENCES employee_role(id) ON DELETE SET NULL
+    manager_id INT
 );
 
 CREATE TABLE employee_role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
-     CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    department_id INT
 );
 
 CREATE TABLE department (
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Jake', 'Smith', 57, NULL),
 ('Joseph', 'Jones', 4, NULL);
